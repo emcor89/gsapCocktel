dcedd7c13fdbae621c2210d1678d1dfa9f397be9
emcor89 <emcor89@gmail.com>
2026-04-12 18:10:07 -0500

    add cocktail video

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -91,7 +91,7 @@ const Hero = () => {
 
             </section>
             <div className='video absolute inset-0'>
-                <video ref={videoRef} src="/videos/input.mp4" muted playsInline preload='auto'/>
+                <video ref={videoRef} src="/videos/output.mp4" muted playsInline preload='auto'/>
                    
             </div>
         </>
